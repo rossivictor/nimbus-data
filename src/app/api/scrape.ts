9d1aaf4a9e7631c0ccc2a0442ab23ba464c8a60d
Victor Rossi <rossivictor90@gmail.com>
2024-06-21 16:35:25 -0300

    some fixes

--- src/app/api/scrape.ts
+++ src/app/api/scrape.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 
-export async function POST(req: NextRequest) {
+export default async function POST(req: NextRequest) {
   const { url } = await req.json();
 
   try {
